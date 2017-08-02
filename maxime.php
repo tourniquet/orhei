e564a273c201b2642c79b46cacdc79d50d93d19e
Ion Prodan <admyn3d@gmail.com>
2017-08-02 21:12:23 +0100

    first commit

---
+++ maxime.php
@@ -0,0 +1,25 @@
+﻿<?php
+$handle = @fopen("maxime.txt", "r");
+$i = -1;
+if($handle)
+{
+        while(!feof($handle))
+        {
+                $i++;
+                $buffer= fgets($handle, 4096);
+                if($buffer != NULL)
+                {
+                        $maxima[$i] = $buffer;
+                }
+        }
+}
+
+
+//print_r($maxima);
+
+$x = rand(0, count($maxima)-1);
+
+$maxima = explode("|", $maxima[$x]);
+
+//print_r($maxima);
+?>
